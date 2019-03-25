1d97c57013b2b9ea5f47ddd18d828904261f85d5
Charlie <cr646531@gmail.com>
2019-03-25 13:54:12 -0400

    changed User model to Player model && added Team model

--- db/index.js
+++ db/index.js
@@ -1,22 +1,34 @@
 const Sequelize = require('sequelize');
 const db = new Sequelize(process.env.DATABASE_URL);
 
-const User = db.define('User', {
+const Player = db.define('player', {
+    name: Sequelize.STRING
+});
+
+const Team = db.define('team', {
     name: Sequelize.STRING
 });
 
 const sync = ()=> db.sync({ force: true });
 const seed = ()=> {
     return Promise.all([
-        User.create({ name: 'Andy' }),
-        User.create({ name: 'Bill' }),
-        User.create({ name: 'Chris' })
-    ]);
+        Player.create({ name: 'Andy' }),
+        Player.create({ name: 'Bill' }),
+        Player.create({ name: 'Chris' })     
+    ])
+        .then(()=> {
+            return Promise.all([
+                Team.create({ name: 'Giants' }),
+                Team.create({ name: 'Jets' }),
+                Team.create({ name: 'Bills' })
+            ]);
+        });
 };
 
 module.exports = {
     models: {
-        User
+        Player,
+        Team
     },
     sync,
     seed
--- test/models.spec.js
+++ test/models.spec.js
@@ -1,14 +1,18 @@
 const expect = require('chai').expect;
 const db = require('../db');
-const { User } = db.models;
+const { Player, Team } = db.models;
 
-describe('users', ()=> {
+describe('database is synced and seeded', ()=> {
     beforeEach(()=> {
         return db.sync()
             .then(()=> db.seed());
     });
-    it('there are 3 users', ()=> {
-        return User.findAll({})
-            .then( users => expect(users.length).to.equal(3));
+    it('there are 3 players', ()=> {
+        return Player.findAll({})
+            .then( players => expect(players.length).to.equal(3));
     });
+    it('there are 3 teams', ()=> {
+        return Team.findAll({})
+            .then(teams => expect(teams.length).to.equal(3));
+    })
 })
