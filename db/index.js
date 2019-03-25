4d91275c3342b149fdbc298e897a20009b9e8656
Charlie <cr646531@gmail.com>
2019-03-25 13:40:42 -0400

    writing database tests for users

--- db/index.js
+++ db/index.js
@@ -2,12 +2,16 @@ const Sequelize = require('sequelize');
 const db = new Sequelize(process.env.DATABASE_URL);
 
 const User = db.define('User', {
-
+    name: Sequelize.STRING
 });
 
 const sync = ()=> db.sync({ force: true });
 const seed = ()=> {
-
+    return Promise.all([
+        User.create({ name: 'Andy' }),
+        User.create({ name: 'Bill' }),
+        User.create({ name: 'Chris' })
+    ]);
 };
 
 module.exports = {
